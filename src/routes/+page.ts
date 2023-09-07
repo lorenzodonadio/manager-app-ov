8083b06a8c10dc9962237ff01f4a62df905dd02d
lorenzodonadio <lorenzo.donadio@epfl.ch>
2023-09-08 00:31:12 +0200

    landing page

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,6 +2,6 @@ import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ parent }) => {
 	const p = await parent();
-	if (!p.session) throw redirect(300, '/auth/login');
-	throw redirect(301, '/manager-hub');
+	if (p.session) throw redirect(301, '/manager-hub');
+	return {};
 };
